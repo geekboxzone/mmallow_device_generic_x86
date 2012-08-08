11c90e9d26fd2dfc7554964a90096bc0d96234d8
Keun young Park <keunyoung@google.com>
2012-08-08 12:51:00 -0700

    add mini_x86 configuration for PDK
    - For x86 PDK release from jb-mr1
    
    Change-Id: I78ec0d7f54a1afdbe18888ce14e6ec08dfb0f3bf

---
+++ BoardConfig.mk
@@ -0,0 +1,18 @@
+#
+# Product-specific compile-time definitions.
+#
+
+# The generic product target doesn't have any hardware-specific pieces.
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
+
+TARGET_ARCH := x86
+TARGET_CPU_ABI := x86
+TARGET_CPU_SMP := true
+
+SMALLER_FONT_FOOTPRINT := true
+MINIMAL_FONT_FOOTPRINT := true
+# Some framework code requires this to enable BT
+BOARD_HAVE_BLUETOOTH := true
+
+WPA_SUPPLICANT_VERSION := VER_0_8_X
