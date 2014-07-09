770d2c13c1211222cc71f6ec67e8ff4f7a8fad52
Nick Kralevich <nnk@google.com>
2014-07-09 10:22:23 -0700

    x86 should inherit from x86 emulator rules
    We were only inheriting from the generic emulator rules,
    not the x86 rules too. This was causing lots of SELinux denials.
    
    Bug: 16101432
    Change-Id: Ib6f1f01c20c857e0ba97747e6a390281d319e1d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,14 +36,19 @@ BOARD_FLASH_BLOCK_SIZE := 512
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 
 BOARD_SEPOLICY_DIRS += build/target/board/generic/sepolicy
+BOARD_SEPOLICY_DIRS += build/target/board/generic_x86/sepolicy
+
 BOARD_SEPOLICY_UNION += \
         bootanim.te \
         device.te \
         domain.te \
         file.te \
         file_contexts \
+        healthd.te \
+        installd.te \
         qemud.te \
         rild.te \
         shell.te \
         surfaceflinger.te \
-        system_server.te
+        system_server.te \
+        zygote.te
